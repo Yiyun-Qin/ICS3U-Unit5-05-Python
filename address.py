e87bec49319704b6471256f2d01427d9a28f1ce8
Yiyun Qin <yiyun.qin@mths.ca>
2022-05-20 18:47:11 +0000

    address

--- address.py
+++ address.py
@@ -5,7 +5,7 @@
 # This is the math program, formatting the address
 
 
-def MailAddress(
+def mail_address(
     name, street_number, street, city, province, postal_code, apartment_number=None
 ):
     # This function returns the string in mail format
@@ -69,7 +69,7 @@ def main():
         # call functions
         if apartment_number_string != None:
             appartment_number = int(apartment_number_string)
-            mail = MailAddress(
+            mail = mail_address(
                 name=full_name,
                 apartment_number=appartment_number,
                 street_number=street_number,
@@ -79,7 +79,7 @@ def main():
                 postal_code=postal_code,
             )
         else:
-            mail = MailAddress(
+            mail = mail_address(
                 name=full_name,
                 street_number=street_number,
                 street=street_name,
